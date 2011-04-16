8df8c116875bf886ef0e6c455ae9c05b9731d398
diversen <dennis.iversen@gmail.com>
2011-04-16 15:19:59 +0200

    lang. ini setting for num chars in search rpc.

--- rpc.php
+++ rpc.php
@@ -3,10 +3,15 @@
 $db = new db();
 
 if(isset($_GET['term'])) {
-    $queryString = cos_htmlentities($_GET['term']);
+    $queryString = html::entitiesEncode($_GET['term']);
+    $at_least = get_module_ini('tags_min_chars');
+    if (!isset($at_least)) {
+        $at_least = 2;
+    }
 
-    if(strlen($queryString) > 2) {
+    if(strlen($queryString) > $at_least) {
         $query = "SELECT id, title as label FROM tags WHERE title LIKE ". db::$dbh->quote("%" . $queryString . "%");
+        error_log($query);
         $rows = $db->selectQuery($query);
         $json = json_encode($rows);
         echo $json;
--- view.php
+++ view.php
@@ -2,6 +2,6 @@
 
 $id = tags::getEntryId();
 $tag = tags::getTagSingle($id);
-//print_r($tag);
+
 $refs = tags::getAllReferenceTagWithoutReference($tag['id'], 0, 10);
-print_r($refs);
+
