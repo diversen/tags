749661fc7f5b4e6c216ac2e14eb4995a3ce3d540
Dennis Iversen <dennis.iversen@gmail.com>
2014-02-12 18:47:57 +0100

    add simple api

---
+++ api/module.php
@@ -0,0 +1,32 @@
+<?php
+
+class tags_api {
+    
+    /**
+     * get tags from /tags/api/[action]/1/1+2
+     */
+    public function getTags () {
+        $tags = uri::fragment(4);
+        $ary = explode("-", $tags);
+        return $ary;
+        
+    }
+    
+    public function deleteTagsAndReferences () {
+        $ary = $this->getTags();
+        foreach ($ary as $val) {
+            db_q::delete('tags_reference')->filter('tags_id =', $val)->exec();
+            db_q::delete('tags')->filter('id =', $val)->exec();
+        }
+    }
+    
+    public function deleteAction () {
+        if (session::isSuper()) {
+            $this->deleteTagsAndReferences();
+        } else {
+            moduleloader::setStatus(403);
+            error_module::$message = lang::translate('Not sufficient privileges. Super user is required');
+            return;
+        }
+    }
+}
