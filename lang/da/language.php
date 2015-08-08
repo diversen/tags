03ca0abf84eadbf22e06d7cca66cf1ddef1c80d6
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-08 15:20:25 +0200

    lang

---
+++ lang/da/language.php
@@ -0,0 +1,25 @@
+<?php
+
+$LANG = array();
+
+$LANG['No title'] = 'Ingen titel';
+$LANG['Add another'] = 'Tilføje en anden';
+$LANG['Tag has been added'] = 'Tag er blevet tilføjet';
+$LANG['Tag has been updated'] = 'Tag er blevet opdateret';
+$LANG['Tag has been deleted'] = 'Tag er blevet slettet';
+$LANG['Edit'] = 'Edit';
+$LANG['Delete'] = 'Slet';
+$LANG['Tags'] = 'Tags';
+$LANG['Tags has been deleted!'] = 'Tags er blevet slettet!';
+$LANG['Not sufficient privileges. Super user is required'] = 'Ikke tilstrækkelige rettigheder. Super bruger er nødvendig';
+$LANG['Delete tag'] = 'Slet tag';
+$LANG['Add a tag'] = 'Tilføj et tag';
+$LANG['Title'] = 'Titel';
+$LANG['Is a main tag'] = 'Er en vigtig tag';
+$LANG['Description'] = 'Beskrivelse';
+$LANG['Submit'] = 'Indsend';
+$LANG['Edit tag'] = 'Rediger tag';
+$LANG['Update'] = 'Opdatering';
+$LANG['Main tags'] = 'Vigtigste tags';
+$LANG['List tags'] = 'Liste tags';
+$LANG['Add tag'] = 'Tilføj tag';
