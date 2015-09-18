771e9540220a2e4400efe6f2cba40874c16eb26f
Dennis Iversen <dennis.iversen@gmail.com>
2015-09-18 20:26:26 +0200

    lang

--- lang/en/language.php
+++ lang/en/language.php
@@ -2,6 +2,20 @@
 
 $LANG = array();
 
+// Translation of file modules/tags/blocks/tags.inc
+
+$LANG['Main tags'] = 'Main tags';
+// Translation of file modules/tags/api/module.php
+
+$LANG['Tags has been deleted!'] = 'Tags has been deleted!';
+$LANG['Not sufficient privileges. Super user is required'] = 'Not sufficient privileges. Super user is required';
+// Translation of file modules/tags/install.inc
+
+$LANG['Tags'] = 'Tags';
+// Translation of file modules/tags/menu.inc
+
+$LANG['List tags'] = 'List tags';
+$LANG['Add tag'] = 'Add tag';
 // Translation of file modules/tags/module.php
 
 $LANG['No title'] = 'No title';
@@ -12,14 +26,13 @@ $LANG['Tag has been deleted'] = 'Tag has been deleted';
 $LANG['Edit'] = 'Edit';
 $LANG['Delete'] = 'Delete';
 $LANG['Tags'] = 'Tags';
-// Translation of file modules/tags/api/module.php
+// Translation of file modules/tags/views/edit.inc
 
-$LANG['Tags has been deleted!'] = 'Tags has been deleted!';
-$LANG['Not sufficient privileges. Super user is required'] = 'Not sufficient privileges. Super user is required';
-// Translation of file modules/tags/views/delete.inc
-
-$LANG['Delete tag'] = 'Delete tag';
-$LANG['Delete'] = 'Delete';
+$LANG['Edit tag'] = 'Edit tag';
+$LANG['Title'] = 'Title';
+$LANG['Is a main tag'] = 'Is a main tag';
+$LANG['Description'] = 'Description';
+$LANG['Update'] = 'Update';
 // Translation of file modules/tags/views/add.inc
 
 $LANG['Add a tag'] = 'Add a tag';
@@ -28,20 +41,7 @@ $LANG['Is a main tag'] = 'Is a main tag';
 $LANG['Description'] = 'Description';
 $LANG['Add another'] = 'Add another';
 $LANG['Submit'] = 'Submit';
-// Translation of file modules/tags/views/edit.inc
+// Translation of file modules/tags/views/delete.inc
 
-$LANG['Edit tag'] = 'Edit tag';
-$LANG['Title'] = 'Title';
-$LANG['Is a main tag'] = 'Is a main tag';
-$LANG['Description'] = 'Description';
-$LANG['Update'] = 'Update';
-// Translation of file modules/tags/blocks/tags.inc
-
-$LANG['Main tags'] = 'Main tags';
-// Translation of file modules/tags/install.inc
-
-$LANG['Tags'] = 'Tags';
-// Translation of file modules/tags/menu.inc
-
-$LANG['List tags'] = 'List tags';
-$LANG['Add tag'] = 'Add tag';
+$LANG['Delete tag'] = 'Delete tag';
+$LANG['Delete'] = 'Delete';
