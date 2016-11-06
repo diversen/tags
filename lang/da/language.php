e9db6c962726c92f30bf5b1d7c9f9e296d5bbac7
Dennis Iversen <dennis.iversen@gmail.com>
2016-11-06 14:15:54 +0100

    up lang

--- lang/da/language.php
+++ lang/da/language.php
@@ -2,6 +2,12 @@
 
 $LANG = array();
 
+$LANG['Tags has been deleted!'] = 'Tags er blevet slettet!';
+$LANG['Not sufficient privileges. Super user is required'] = 'Ikke tilstrækkelige rettigheder. Super bruger er nødvendig';
+$LANG['Main tags'] = 'Vigtigste tags';
+$LANG['Tags'] = 'Tags';
+$LANG['List tags'] = 'Liste tags';
+$LANG['Add tag'] = 'Tilføj tag';
 $LANG['No title'] = 'Ingen titel';
 $LANG['Add another'] = 'Tilføje en anden';
 $LANG['Tag has been added'] = 'Tag er blevet tilføjet';
@@ -9,17 +15,11 @@ $LANG['Tag has been updated'] = 'Tag er blevet opdateret';
 $LANG['Tag has been deleted'] = 'Tag er blevet slettet';
 $LANG['Edit'] = 'Edit';
 $LANG['Delete'] = 'Slet';
-$LANG['Tags'] = 'Tags';
-$LANG['Tags has been deleted!'] = 'Tags er blevet slettet!';
-$LANG['Not sufficient privileges. Super user is required'] = 'Ikke tilstrækkelige rettigheder. Super bruger er nødvendig';
-$LANG['Delete tag'] = 'Slet tag';
 $LANG['Add a tag'] = 'Tilføj et tag';
 $LANG['Title'] = 'Titel';
 $LANG['Is a main tag'] = 'Er en vigtig tag';
 $LANG['Description'] = 'Beskrivelse';
 $LANG['Submit'] = 'Indsend';
+$LANG['Delete tag'] = 'Slet tag';
 $LANG['Edit tag'] = 'Rediger tag';
 $LANG['Update'] = 'Opdatering';
-$LANG['Main tags'] = 'Vigtigste tags';
-$LANG['List tags'] = 'Liste tags';
-$LANG['Add tag'] = 'Tilføj tag';
--- lang/en/language.php
+++ lang/en/language.php
@@ -2,6 +2,24 @@
 
 $LANG = array();
 
+// Translation of file modules/tags/api/module.php
+
+$LANG['Tags has been deleted!'] = 'Tags has been deleted!';
+$LANG['Not sufficient privileges. Super user is required'] = 'Not sufficient privileges. Super user is required';
+
+// Translation of file modules/tags/blocks/tags.inc
+
+$LANG['Main tags'] = 'Main tags';
+
+// Translation of file modules/tags/install.inc
+
+$LANG['Tags'] = 'Tags';
+
+// Translation of file modules/tags/menu.inc
+
+$LANG['List tags'] = 'List tags';
+$LANG['Add tag'] = 'Add tag';
+
 // Translation of file modules/tags/module.php
 
 $LANG['No title'] = 'No title';
@@ -12,14 +30,7 @@ $LANG['Tag has been deleted'] = 'Tag has been deleted';
 $LANG['Edit'] = 'Edit';
 $LANG['Delete'] = 'Delete';
 $LANG['Tags'] = 'Tags';
-// Translation of file modules/tags/api/module.php
 
-$LANG['Tags has been deleted!'] = 'Tags has been deleted!';
-$LANG['Not sufficient privileges. Super user is required'] = 'Not sufficient privileges. Super user is required';
-// Translation of file modules/tags/views/delete.inc
-
-$LANG['Delete tag'] = 'Delete tag';
-$LANG['Delete'] = 'Delete';
 // Translation of file modules/tags/views/add.inc
 
 $LANG['Add a tag'] = 'Add a tag';
@@ -28,6 +39,12 @@ $LANG['Is a main tag'] = 'Is a main tag';
 $LANG['Description'] = 'Description';
 $LANG['Add another'] = 'Add another';
 $LANG['Submit'] = 'Submit';
+
+// Translation of file modules/tags/views/delete.inc
+
+$LANG['Delete tag'] = 'Delete tag';
+$LANG['Delete'] = 'Delete';
+
 // Translation of file modules/tags/views/edit.inc
 
 $LANG['Edit tag'] = 'Edit tag';
@@ -35,13 +52,3 @@ $LANG['Title'] = 'Title';
 $LANG['Is a main tag'] = 'Is a main tag';
 $LANG['Description'] = 'Description';
 $LANG['Update'] = 'Update';
-// Translation of file modules/tags/blocks/tags.inc
-
-$LANG['Main tags'] = 'Main tags';
-// Translation of file modules/tags/install.inc
-
-$LANG['Tags'] = 'Tags';
-// Translation of file modules/tags/menu.inc
-
-$LANG['List tags'] = 'List tags';
-$LANG['Add tag'] = 'Add tag';
